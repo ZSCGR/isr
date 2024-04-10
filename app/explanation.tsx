6e3217ab3b663b01407d0ce96e9b18302a6a57ab
ZSCGR <932478692@qq.com>
2024-04-10 21:24:18 +0800

    Update explanation.tsx

--- app/explanation.tsx
+++ app/explanation.tsx
@@ -4,23 +4,23 @@ export default function Explanation() {
   return (
     <div className={styles.explanation}>
       <p>
-        This app demonstrates <b>*On-Demand ISR support*</b> in Next.js (
+        此项目使用了 Next.js，托管在Vercel上 (
         <a
-          href="https://github.com/ZSCGR/isr"
+          href="https://github.com/leerob/on-demand-isr"
           target="_blank"
           rel="noreferrer"
         >
-          view source
+          点击查看源代码
         </a>
         ).
       </p>
 
       <details className={styles.details}>
         <summary>
-          <span>How does this work?</span>
+          <span>它的原理是什么？</span>
         </summary>
         <p>
-          When issues on the `
+          当 `
           <a
             href="https://github.com/ZSCGR/isr"
             target="_blank"
@@ -28,9 +28,9 @@ export default function Explanation() {
           >
             isr
           </a>
-          ` repo change (get created, commented on, deleted, etc), GitHub fires
-          off a webhook and the impacted pages get re-rendered and pushed to the
-          edge, on demand. The webhook on the Next.js app side executes a new{' '}
+          ` 这个仓库的问题（issues）发生变化 (包括创建、注释、删除等)时, GitHub 会触发
+          一个 webhook，受影响的页面会按需重新渲染并推送到边缘。Next.js 应用程序端的
+          webhook 会执行一个新的{' '}
           <a
             href="https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#revalidating-data"
             target="_blank"
@@ -38,27 +38,27 @@ export default function Explanation() {
           >
             <code>`revalidatePath()`</code>
           </a>{' '}
-          API call.
+          API 调用。
         </p>
       </details>
 
       <details className={styles.details}>
         <summary>
-          <span>Didn&#39;t this exist already?</span>
+          <span>这不是已经有了吗？</span>
         </summary>
 
         <p>
-          Unlike <code>`revalidate`</code>{' '}
+          不像 <code>`revalidate`</code>{' '}
           <a
             href="https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#time-based-revalidation"
             target="_blank"
             rel="noreferrer"
           >
-            with a time interval
+            有时间间隔
           </a>
-          , a Serverless Function is invoked{' '}
-          <em>_only when content changes_</em>, making it faster for the user
-          (they see changes immediately), and more cost-efficient for owners.
+          ，无服务器函数只有在{' '}
+          <em>内容发生变化时</em>才会被调用，这对用户来说更快
+          （他们能立即看到变化），对所有者来说也更具有成本效益。
         </p>
       </details>
 
